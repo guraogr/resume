4fa3b83dbe8c5029c030bcbe20ae949f3765f772
guraogr <guraogr@gmail.com>
2023-02-09 13:10:11 +0900

    feat: DefaultLayoutの実装 & プロフィールページの作成中

---
+++ src/layouts/Default.tsx
@@ -0,0 +1,8 @@
+import { Box } from '@mantine/core'
+import type { PropsWithChildren } from 'react'
+
+import { contentsWidth } from 'constans'
+
+export const DefaultLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  return <Box sx={{ maxWidth: contentsWidth, margin: 'auto' }}>{children}</Box>
+}
