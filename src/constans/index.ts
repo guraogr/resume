dcf5be5cb165feb82e531c01fa2eb94d37fc8df8
guraogr <guraogr@gmail.com>
2023-02-10 16:00:40 +0900

    fix: constansフォルダをsrcに移動

---
+++ src/constans/index.ts
@@ -0,0 +1,14 @@
+export const APP_NAME = "Ogura's Portfolio"
+export const APP_DESCRIPTION = ''
+
+export const BREAKPOINT_MAP = {
+  xs: 576,
+  sm: 768,
+  md: 992,
+  lg: 1140,
+  xl: 1400,
+}
+
+export const contentsWidth = 1140
+
+export const spacing = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56]
