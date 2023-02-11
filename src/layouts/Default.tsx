b79b9c473df86764f992df51821103cfe9938cdd
guraogr <guraogr@gmail.com>
2023-02-11 21:25:03 +0900

    feat: topページの修正

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,8 +1,6 @@
 import { Box } from '@mantine/core'
 import type { PropsWithChildren } from 'react'
 
-import { contentsWidth } from '@/constans'
-
 export const DefaultLayout: React.FC<PropsWithChildren> = ({ children }) => {
-  return <Box sx={{ maxWidth: contentsWidth, margin: 'auto' }}>{children}</Box>
+  return <Box>{children}</Box>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import { Box } from '@mantine/core'
-
-import { Header } from '@/components/Header'
+import ProfilePage from '@/components/page/profile'
+import { Header } from '@/components/ui/Header'
 import { PAGE_TITLE_MAP } from '@/constans/title'
-import { ProfileCard } from '@/features/ProfileCard'
-import { Head } from '@/features/misc/Head'
+import { Head } from '@/features/Misc/Head'
 import { DefaultLayout } from '@/layouts/Default'
 import { clientEnv } from 'env/schema'
 
@@ -32,20 +30,7 @@ const Home: React.FC = () => {
       />
       <Header links={headerLinks} />
       <DefaultLayout>
-        <Box sx={{ maxWidth: 292 }}>
-          <ProfileCard
-            bg={'/images/profile_bg.png'}
-            thumbnail={'/images/my_photo.png'}
-            thumbnailAlt={'おぐらひろきの自画像'}
-            name={'おぐら ひろき'}
-            job={'デザインエンジニア'}
-            githubId={'guraogr'}
-            description={
-              '1999年京都府生まれ。デザインエンジニア。ECCコンピュータ専門学校卒業後、BizReach、YumemiなどのIT企業でデザイナーとしてサービス開発に携わりつつ、個人で数社のスタートアップの事業改善・デザイン・開発など幅広く携わる。'
-            }
-            hashtag={'#BtoB #SaaS #DX #Front-end #PdM #組織 #キャリア'}
-          />
-        </Box>
+        <ProfilePage />
       </DefaultLayout>
     </>
   )
