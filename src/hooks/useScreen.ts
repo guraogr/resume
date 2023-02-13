96e8516b3f0f3874b8df82106cb01ce05b32b994
guraogr <guraogr@gmail.com>
2023-02-13 21:58:50 +0900

    feat: useScreenにstylesを追加

--- src/hooks/useScreen.ts
+++ src/hooks/useScreen.ts
@@ -2,9 +2,18 @@ import { useMediaQuery } from '@mantine/hooks'
 
 import { BREAKPOINT_MAP } from '@/constans'
 
+interface ResponsiveStyles {
+  contentsMaxWidth: number
+  contentsWidth: string
+  direction: 'column' | 'row'
+  align: 'center' | 'left'
+  justify: 'center' | 'left'
+}
+
 type UseScreen = () => {
   isSmartPhoneScreen: boolean
   isTabletScreen: boolean
+  styles: ResponsiveStyles
 }
 
 export const useScreen: UseScreen = () => {
@@ -12,8 +21,16 @@ export const useScreen: UseScreen = () => {
     `(max-width: ${BREAKPOINT_MAP.sm}px)`
   )
   const isTabletScreen = useMediaQuery(`(max-width: ${BREAKPOINT_MAP.md}px)`)
+  const styles = {
+    contentsMaxWidth: isTabletScreen ? 640 : 1140,
+    contentsWidth: isTabletScreen ? '80%' : '90%',
+    direction: isTabletScreen ? ('column' as const) : ('row' as const),
+    align: isTabletScreen ? ('center' as const) : ('left' as const),
+    justify: isTabletScreen ? ('center' as const) : ('left' as const),
+  }
   return {
     isSmartPhoneScreen,
     isTabletScreen,
+    styles,
   }
 }
