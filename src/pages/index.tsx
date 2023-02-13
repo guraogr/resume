412ff47795e98ec0ca903fe9eb97f4e871a5e030
guraogr <guraogr@gmail.com>
2023-02-13 18:46:42 +0900

    fix: grobalNavigationLinksに差し替え

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,12 @@
 import ProfilePage from '@/components/page/profile'
 import { Header } from '@/components/ui/Header'
+import { grobalNavigationLinks } from '@/constans'
 import { PAGE_TITLE_MAP } from '@/constans/title'
 import { Head } from '@/features/Misc/Head'
 import { DefaultLayout } from '@/layouts/Default'
 import { type JobHistries } from '@/models/JobHistory'
 import { type GetListRequestQuery } from '@/models/Request'
 import { clientEnv } from 'env/validators'
-
-const headerLinks = [
-  {
-    link: '/',
-    label: 'プロフィール',
-  },
-  {
-    link: '/project',
-    label: '参画プロジェクト',
-  },
-  {
-    link: '/contact',
-    label: 'お問い合わせ',
-  },
-]
 
 interface Props {
   data: GetListRequestQuery<JobHistries>
@@ -34,7 +20,7 @@ const Home: React.FC<Props> = (props) => {
         pageUrl={clientEnv.NEXT_PUBLIC_APP_URL}
         type="website"
       />
-      <Header links={headerLinks} />
+      <Header links={grobalNavigationLinks} />
       <DefaultLayout>
         <ProfilePage />
       </DefaultLayout>
