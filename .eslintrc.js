19a971fdece1a8e4dc8cd3eaa55cd563cbc04a8d
guraogr <guraogr@gmail.com>
2023-02-13 11:17:28 +0900

    feat: プラグインの追加 | 軽微な修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'unused-imports/no-unused-imports': 'error',
+    '@typescript-eslint/prefer-optional-chain': 'warn',
     semi: ['error', 'never'],
     'require-jsdoc': ['off'],
     'import/order': [
--- env/schema.ts
+++
@@ -1,9 +0,0 @@
-import * as z from 'zod'
-
-export const clientSchema = z.object({
-  NEXT_PUBLIC_APP_URL: z.string().url(),
-})
-
-export const clientEnv = clientSchema.parse({
-  NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-})
