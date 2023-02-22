42a285021a38a61738d92865866d4fdd28ef622a
guraogr <guraogr@gmail.com>
2023-02-22 13:38:22 +0900

    feat: envのzod追加

--- env/validators.ts
+++ env/validators.ts
@@ -3,11 +3,13 @@ import * as z from 'zod'
 export const clientSchema = z.object({
   NEXT_PUBLIC_APP_URL: z.string().url(),
   NEXT_PUBLIC_API_KEY: z.string(),
-  NEXT_PUBLIC_BASE_URL: z.string().url(),
+  NEXT_PUBLIC_SERVICE_DOMAIN: z.string(),
+  NEXT_PUBLIC_MICROCMS_URL: z.string().url(),
 })
 
 export const clientEnv = clientSchema.parse({
   NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
-  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  NEXT_PUBLIC_MICROCMS_URL: process.env.NEXT_PUBLIC_MICROCMS_URL,
+  NEXT_PUBLIC_SERVICE_DOMAIN: process.env.NEXT_PUBLIC_SERVICE_DOMAIN,
 })
